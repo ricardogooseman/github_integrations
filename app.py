cfa5ff3fe106fb4c5d4ef0d186999e628e287937
ricardogooseman <raguzman9718@gmail.com>
2024-04-14 12:19:20 -0700

    testing lab

---
+++ app.py
@@ -0,0 +1,15 @@
+# from flask import Flask
+
+# app = Flask(__name__)
+
+
+# @app.route("/")
+# def hello():
+#     return "Hello Everyone!"
+
+
+# if __name__ == "__main__":
+#     app.run()
+
+def adds_one(number):
+    return number + 1
---
+++ testapp.py
@@ -0,0 +1,5 @@
+from app import adds_one
+
+
+def test_adds_one():
+    assert adds_one(1) == 2
